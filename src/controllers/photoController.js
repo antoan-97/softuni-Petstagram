801c0d292927e2d6ff23e4120e204f0d298eab7e
antoan-97 <antoan.todorov@abv.bg>
2024-05-19 13:21:24 +0300

    add post/get create routers

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,7 +1,23 @@
+const { getErrorMessage } = require('../utils/errorHelper');
+const photoManager = require('../managers/photoManager');
+
 const router = require('express').Router();
 
-router.get('/create', (req,res) =>{
+router.get('/create', (req, res) => {
     res.render('photos/create');
 });
+
+router.post('/create', async (req, res) => {
+    const photoData = {
+        ...req.body,
+        owner:req.user._id,
+    }
+    try {
+        await photoManager.create(photoData);
+        res.redirect('/photos');
+    } catch (err) {
+        res.render('photos/create', { error: getErrorMessage(err) })
+    }
+})
 
 module.exports = router;
