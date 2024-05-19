e451877b3f6a53db02d3d8da530bdd4e386f2d88
antoan-97 <antoan.todorov@abv.bg>
2024-05-19 14:06:04 +0300

    add comments to photoSchema

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -25,7 +25,18 @@ const photoSchema = new mongoose.Schema({
     type:mongoose.Types.ObjectId,
     ref:'User',
   },
-})
+  comments:{
+    user:{
+        type:mongoose.Types.ObjectId,
+        required:true,
+        ref:'User',
+    },
+    message:{
+        type:String,
+        required:[true, 'Comment message its required!'],
+    },
+  },
+});
 
 const Photo = mongoose.model('Photo' , photoSchema);
 
