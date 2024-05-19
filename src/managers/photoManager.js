4e5acdb2268250c1825c904e5026e9917f1e14d8
antoan-97 <antoan.todorov@abv.bg>
2024-05-19 16:43:02 +0300

    make addComment manager

--- src/managers/photoManager.js
+++ src/managers/photoManager.js
@@ -4,8 +4,14 @@ exports.create = (photoData) => Photo.create(photoData);
 
 exports.getAll = () => Photo.find().populate('owner');
 
-exports.getOne = (photoId) => Photo.findById(photoId).populate('owner');
+exports.getOne = (photoId) => Photo.findById(photoId).populate('owner').populate('comments.user');
 
 exports.edit = (photoId, photoData) => Photo.findByIdAndUpdate(photoId, photoData);
 
 exports.delete = (photoId) => Photo.findByIdAndDelete(photoId);
+
+exports.addComment = async (photoId, commentData) => {
+    const photo = await Photo.findById(photoId);
+    photo.comments.push(commentData);
+    return photo.save()
+}; 
