2934837610e2b76d8bb6abe6737caaf6b7f730bb
antoan-97 <antoan.todorov@abv.bg>
2024-05-19 14:53:30 +0300

    create delete manager function

--- src/managers/photoManager.js
+++ src/managers/photoManager.js
@@ -5,3 +5,5 @@ exports.create = (photoData) => Photo.create(photoData);
 exports.getAll = () => Photo.find().populate('owner');
 
 exports.getOne = (photoId) => Photo.findById(photoId).populate('owner');
+
+exports.delete = (photoId) => Photo.findByIdAndDelete(photoId);
