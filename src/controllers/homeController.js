eedc2c8b3b91f5bc537ef5469dfc1ce766e9ed46
antoan-97 <antoan.todorov@abv.bg>
2024-05-21 21:50:26 +0300

    render profile page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
+const photoManager = require('../managers/photoManager');
 
 router.get('/', (req,res) =>{
-    res.render('home')
+    res.render('home');
 });
 
 router.get('/404', (req,res) =>{
-    res.render('404')
-})
+    res.render('404');
+});
+
+router.get('/profile', async  (req,res) =>{
+const photos = await photoManager.getByOwner(req.user._id).lean();
+
+    res.render('profile', { photos, photosCount: photos.length });
+});
 
 module.exports = router;
